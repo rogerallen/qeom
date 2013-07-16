338f40058b75312c4db3ce85a68bd2b710789b2d
Roger Allen <rallen@gmail.com>
2013-07-15 22:37:32 -0700

    tweaks to updating

--- src/qeom/q002.clj
+++ src/qeom/q002.clj
@@ -6,21 +6,22 @@
 
 (defn setup []
   (smooth)
-  (frame-rate 0.2)
+  (frame-rate 30)
   (background 220 220 200)
-  (stroke 0)
   (stroke-weight 5)
+  (stroke 0)
   (fill 200))
 
 (defn draw []
   (background 220 220 200)
   (translate 450 450)
   (rotate (/ PI 4))
-  (dotimes [i 100]
+  (random-seed (int (/ (millis) 10000)))
+  (dotimes [i (* 20 6)]
     (let [x0 -200
-          j (- (rand-int 20) 10)
+          j (- (int (random 20)) 10)
           y0 (* 20 j)]
-      (if (= 0 (rand-int 2))
+      (if (= 0 (int (random 2)))
         (rect x0 y0 400 20)
         (rect y0 x0 20 400)))))
 
