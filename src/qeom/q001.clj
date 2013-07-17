a0d7be3219b74ef9b71ac9a54c72112811e00f45
Roger Allen <rallen@gmail.com>
2013-07-17 08:16:11 -0700

    Revamp this lame attempt.  Now inspired by geometrydaily 474

--- src/qeom/q001.clj
+++ src/qeom/q001.clj
@@ -1,32 +1,41 @@
+;; inspired by
+;; http://geometrydaily.tumblr.com/post/54427954169/474-trapped-a-new-minimal-geometric-composition
 (ns qeom.q001
   (:use quil.core))
 
 (defn setup []
   (smooth)
-  (frame-rate 5)
-  (background 200))
+  (frame-rate 30))
+
+(defn random-bool []
+  (= 1 (int (random 0 2))))
 
 (defn prim
   [x y w rot]
   (let [r (* (Math/sqrt 3) (/ w 2))
-        x0 (+ x (* r (Math/cos (+ rot (/ (* 0 2 Math/PI) 3)))))
-        y0 (+ y (* r (Math/sin (+ rot (/ (* 0 2 Math/PI) 3)))))
-        x1 (+ x (* r (Math/cos (+ rot (/ (* 1 2 Math/PI) 3)))))
-        y1 (+ y (* r (Math/sin (+ rot (/ (* 1 2 Math/PI) 3)))))
-        x2 (+ x (* r (Math/cos (+ rot (/ (* 2 2 Math/PI) 3)))))
-        y2 (+ y (* r (Math/sin (+ rot (/ (* 2 2 Math/PI) 3)))))]
+        [x0 x1 x2] (map #(+ x (* r (Math/cos (+ rot (/ (* % 2 Math/PI) 3))))) [0 1 2])
+        [y0 y1 y2] (map #(+ y (* r (Math/sin (+ rot (/ (* % 2 Math/PI) 3))))) [0 1 2])]
     (triangle x0 y0 x1 y1 x2 y2)))
 
-(defn draw []
-  (stroke (random 255))
-  (stroke-weight (random 10))
-  (fill (random 255))
+(defn prims
+  [x y]
+  (doall
+   (doseq [sfn (if (random-bool) [first second] [second first])]
+     (let [rot (sfn [0 PI])
+           col (sfn [[128 64 52] [166 184 166]])]
+       (apply fill col)
+       (prim x y 34 rot)))))
 
-  (let [w (random 50 100)
-        rot (random Math/PI)
-        x (* 10 (random (/ (width) 10)))
-        y (* 10 (random (/ (height) 10)))]
-    (prim x y w rot)))
+(defn draw []
+  (background 230)
+  (translate 100 100)
+  ;;(stroke (random 255))
+  (stroke-weight 0)
+  (random-seed (int (/ (millis) 10000)))
+  (doall
+   (map (fn [[i j]] (prims (* i (* (Math/sqrt 3) 26)) (* j 26)))
+        (filter (fn [[i j]] (= 1 (mod (+ i j) 2)))
+                (for [i (range 16) j (range (dec (* (Math/sqrt 3) 16)))] [i j])))))
 
 (defn run [title]
   (defsketch doodle :title (str title) :setup setup :draw draw :size [900 900])
