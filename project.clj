9a5a48556683237dd3683c07bd90a69c3601d376
Roger Allen <rallen@gmail.com>
2014-05-21 12:41:09 -0700

    upgrade to quil 2.0 / processing 2.0 !

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [quil "1.6.0"]]
+                 [quil "2.0.0"]]
   :main qeom.core)
